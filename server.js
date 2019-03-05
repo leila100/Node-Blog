65ebbbff4b3fc8e72771c4bae341b2fabb2b56d3
Leila <nb.leila10@gmail.com>
2019-03-04 22:30:06 -0600

    Initial setup of server

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require("express")
 
 const server = express()
+const logger = require("morgan")
+
+server.use(express.json())
+server.use(logger("dev"))
+
+server.get("/", (req, res) => {
+  res.status(200).json("Welcome to Node-Blog")
+})
 
 module.exports = server
